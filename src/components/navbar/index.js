92cf3fcb264ab12e6bfda9098b6669163ca4c590
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-02-19 12:41:27 -0300

    feat:update

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import * as S from './style';
 
 const Navbar = () => {
+
+  
+
     return (
         <>
             <S.Container>
                 <S.Content>
                     <S.Ahref href='/'>Codelândia</S.Ahref >
-                    <S.Ahref  href='/'>blog</S.Ahref>
+                    <S.Ahref href='/'>blog</S.Ahref>
                 </S.Content>
             </S.Container>
         </>
