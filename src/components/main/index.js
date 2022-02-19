93ba13bacd59627b12c75ba508d9b82fb3df6bf6
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-02-19 12:40:46 -0300

    feat: search script added

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -5,6 +5,7 @@ import * as S from './style';
 const Main = () => {
 
     const [dados, setDados] = useState();
+    const [inputValue, setInputValue] = useState('')
 
     useEffect(() => {
         fetch("https://6201763afdf5090017249a1f.mockapi.io/posts")
@@ -12,14 +13,35 @@ const Main = () => {
             .then(json => setDados(json))
     }, []);
 
+    const teste = (event) => {
+        event.preventDefault();
+        setInputValue(event.target.value)
+    };
+
+    const SearchDatasInput = (event) => {
+        event.preventDefault();
+
+        let animals = document.getElementsByClassName("animals");
+        let i = 0;
+        for (i = 0; i < animals.length; i++) {
+            if (!animals[i].innerHTML.toLowerCase().includes(inputValue)) {
+                animals[i].style.display = "none";
+            } else {
+                animals[i].style.display = "block";
+
+            }
+        }
+    };
+
     return (
         <>
             <S.Container>
+                <S.Input type="text" placeholder='Pesquisar titulo...' onKeyUp={SearchDatasInput} onChange={teste} />
                 <S.Content>
                     {dados &&
                         dados.map(item => (
                             <>
-                                <S.Card>
+                                <S.Card className='animals'>
                                     <S.Span>{item.date}</S.Span>
                                     <S.Title>{item.title}</S.Title>
                                     <S.Paragraph>{item.describe}</S.Paragraph>
@@ -28,6 +50,7 @@ const Main = () => {
                         ))
                     }
                 </S.Content>
+
             </S.Container>
         </>
     )
