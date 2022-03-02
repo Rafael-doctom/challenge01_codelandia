b28f90eca296e14da9be65b9b40d7abb46c1fca4
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-03-02 09:41:48 -0300

    feat: dark mode added

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
+
+import { lightTheme, darkTheme } from "./style";
 import * as S from './style';
 
 const Navbar = () => {
 
-  
+    const [themeActually, setEstado] = useState('light');
+
+    const DarkButton = () => {
+        themeActually === "light" ? setEstado("dark") : setEstado("light");
+    };
+
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem("theme");
+        localTheme && setEstado(localTheme);
+    }, []);
 
     return (
         <>
-            <S.Container>
+            <S.Container theme={themeActually === "light" ? lightTheme : darkTheme}>
                 <S.Content>
                     <S.Ahref href='/'>Codelândia</S.Ahref >
-                    <S.Ahref href='/'>blog</S.Ahref>
+                    <S.DarkModeOff onClick={DarkButton} ></S.DarkModeOff>
                 </S.Content>
             </S.Container>
         </>
