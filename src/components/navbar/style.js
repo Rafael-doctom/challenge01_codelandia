f6c6c720cc1d2c1b705136ad7109a207b8f7e568
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-03-02 09:41:38 -0300

    feat: dark mode

--- src/components/navbar/style.js
+++ src/components/navbar/style.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+
 export const Container = styled.div`
-    background: linear-gradient(88.27deg, #574AE8 0%, #3EA1DB 100%);
+    // background: linear-gradient(88.27deg, #574AE8 0%, #3EA1DB 100%);
     height:230px;
     width:100%;
+    background: ${({ theme }) => theme.body};
 
     @media (max-width:768px){
         height:200px;
@@ -47,3 +49,37 @@ export const Ahref = styled.a`
     line-height: 30px;
     color: #FFFFFF;
 `;
+
+export const DarkModeOff = styled.div`
+    display: block;
+    box-sizing: border-box;
+    border-radius: 20px;
+    border:0;
+
+  &:before{
+    display: block;
+    box-sizing: border-box;
+    border-radius: 20px;
+    content: "";
+    position: absolute;
+    width: 1vw;
+    height: 2vh;
+    background: white;
+    top: 6px;
+    left: 5px;
+    }
+
+    border: 1px solid white;
+    position: relative;
+    transform: scale(var(--ggs, 1));
+    width: 4vw;
+    height: 4vh;
+`;
+
+export const lightTheme = {
+    body: "linear-gradient(88.27deg, #574AE8 0%, #3EA1DB 100%)",
+  };
+  
+export const darkTheme = {
+    body: "red",
+};
